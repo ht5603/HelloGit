ebcce15cf5d92dadcfbcda56703f54d6b198b2f8
Mike <ht5603@n56-h146.150.118.dynamic.da.net.tw>
2016-09-03 21:33:29 +0800

    666 resoulve conflict

--- src/HelloConfig.java
+++ src/HelloConfig.java
@@ -1,4 +1,23 @@
 
 public class HelloConfig {
+<<<<<<< HEAD
+=======
+	
+	//develop分支
+	public void delvelopg(){
+		System.out.println("develop brach");
+	}
+	
+	//feature1
+	public void feature1(){
+		System.out.println("feature1 111");
+		System.out.println("feature1 222");
+		System.out.println("feature1 333");
+		
+		System.out.println("feature1 444");
+		System.out.println("feature1 555");
+		System.out.println("feature1 666");
+	}
+>>>>>>> b5a43c4... feature 666
 
 }
